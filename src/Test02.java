7e4dd376050ee91e67815d2e96abb08df999455d
yangliyi <yangliyi314@163.com>
2017-11-29 00:25:44 +0800

    commit Sigleton

--- src/Test02.java
+++ src/Test02.java
@@ -4,25 +4,28 @@
  * Time: 13:58
  * Declaration: All Rights Reserved !!!
  */
+//单例模式    http://wuchong.me/blog/2014/08/28/how-to-correctly-write-singleton-pattern/  写的非常好
 public class Test02 {
 
     /**
-     * 单例模式，懒汉式，线程安全
+     * 单例模式，饿汉式，线程安全
+     * 饿汉  --  一上来就先实例化，但若类没有使用的话，就有点浪费资源类
      */
     public static class Singleton {
-        private final static Singleton INSTANCE = new Singleton();
+        private final static Singleton instance = new Singleton();
 
         private Singleton() {
 
         }
 
         public static Singleton getInstance() {
-            return INSTANCE;
+            return instance;
         }
     }
 
     /**
-     * 单例模式，饿汉式，线程不安全
+     * 单例模式，饱汉式，线程不安全
+     * 饱汉 -- 实例在开始时为空，第一次加载后才实例化  可节约一些资源，但在并发时有可能出现多个单例
      */
     public static class Singleton2 {
         private static Singleton2 instance = null;
@@ -42,7 +45,7 @@ public class Test02 {
 
 
     /**
-     * 单例模式，饿汉式，线程安全，多线程环境下效率不高
+     * 单例模式，饱汉式，线程安全，多线程环境下效率不高
      */
     public static class Singleton3 {
         private static Singleton3 instance = null;
@@ -61,7 +64,9 @@ public class Test02 {
     }
 
     /**
-     * 单例模式，饿汉式，变种，线程安全
+     * 单例模式，汉式，变种，线程安全
+     *
+     * static 应该是在 加载Singleton4的时候就加载了 , 所以我觉得还是饿汉模式，
      */
     public static class Singleton4 {
         private static Singleton4 instance = null;
@@ -81,6 +86,11 @@ public class Test02 {
 
     /**
      * 单例模式，懒汉式，使用静态内部类，线程安全【推荐】
+     * 在这个例子中内部类 SingletonPatterHolder 的静态变量 singletonPattern，
+     * 这个变量是我们需要的那个单例，即外部类 SingletonPattern 的对象，就是那个我们需要的唯一的对象。
+
+     当我们调用 SingletonPattern.getInstance() 时，内部类 SingletonPatternHolder 才会初始化，
+     静态变量 singletonPattern 被创建出来。
      */
     public static class Singleton5 {
         private final static class SingletonHolder {
@@ -99,13 +109,10 @@ public class Test02 {
     /**
      * 静态内部类，使用枚举方式，线程安全【推荐】
      */
-    public enum Singleton6 {
-        INSTANCE;
-
-        public void whateverMethod() {
-
-        }
-    }
+//    public enum Singleton6 {
+//        INSTANCE;
+//
+//    }
 
     /**
      * 静态内部类，使用双重校验锁，线程安全【推荐】
@@ -131,12 +138,12 @@ public class Test02 {
     }
 
     public static void main(String[] args) {
-        System.out.println(Singleton.getInstance() == Singleton.getInstance());
-        System.out.println(Singleton2.getInstance() == Singleton2.getInstance());
-        System.out.println(Singleton3.getInstance() == Singleton3.getInstance());
-        System.out.println(Singleton4.getInstance() == Singleton4.getInstance());
-        System.out.println(Singleton5.getInstance() == Singleton5.getInstance());
-        System.out.println(Singleton6.INSTANCE == Singleton6.INSTANCE);
+        System.out.println(Test02.Singleton.getInstance() == Test02.Singleton.getInstance());
+        System.out.println(Test02.Singleton2.getInstance() == Test02.Singleton2.getInstance());
+        System.out.println(Test02.Singleton3.getInstance() == Test02.Singleton3.getInstance());
+        System.out.println(Test02.Singleton4.getInstance() == Test02.Singleton4.getInstance());
+        System.out.println(Test02.Singleton5.getInstance() == Test02.Singleton5.getInstance());
+//        System.out.println(Singleton6.INSTANCE == Singleton6.INSTANCE);
         System.out.println(Singleton7.getInstance() == Singleton7.getInstance());
     }
 
